dc7924574145857d92d9362238a2f98093749726
Lisa U <lisa.ubbenjans@gmail.com>
2023-06-03 21:22:12 +0200

    Entrega 1 js

---
+++ js/miarchivo.js
@@ -0,0 +1,32 @@
+// Calcular cuota mensual
+function calcularCuota() {
+  const montoPrestamo = parseFloat(prompt("Ingresa el monto del préstamo:"));
+  const meses = parseInt(prompt("Ingresa el número de meses:"));
+
+  if (isNaN(montoPrestamo) || isNaN(meses) || montoPrestamo <= 0 || meses <= 0) {
+    console.warn("Entrada inválida. Por favor, ingresa números válidos.");
+    return;
+  }
+
+  const tasaInteres = 0.05;
+  const tasaInteresMensual = tasaInteres / 12;
+
+  const cuotaMensual = montoPrestamo * (tasaInteresMensual / (1 - Math.pow(1 + tasaInteresMensual, -meses)));
+
+  console.log("Cuotas Mensuales:");
+  let mes = 1;
+  let montoRestante = montoPrestamo;
+
+  while (mes <= meses) {
+    const interesMensual = montoRestante * tasaInteresMensual;
+    const capitalMensual = cuotaMensual - interesMensual;
+    
+    montoRestante -= capitalMensual;
+    
+    console.log("Mes " + mes + ": Cuota: $" + cuotaMensual.toFixed(2) + " - Interés: $" + interesMensual.toFixed(2) + " - Capital: $" + capitalMensual.toFixed(2) + " - Monto Restante: $" + montoRestante.toFixed(2));
+
+    mes++;
+  }
+}
+
+console.log("Para usar la Calculadora de Préstamos, por favor llama a la función 'calcularCuota()'.");
